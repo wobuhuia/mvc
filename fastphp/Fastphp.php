4b7b91e53c2895806537341bf28961b245c0331a
wobuhuia <xiaogu971017@163.com>
2018-03-13 22:43:54 +0800

    完善

--- fastphp/Fastphp.php
+++ fastphp/Fastphp.php
@@ -45,11 +45,140 @@ class Fastphp
 		if ($url) {
 			// 使用"/"分割字符串,并保存在数组中
 			$urlArray = explode('/', $url);
-			//删除空的数组元素
+			// 删除空的数组元素
 			$urlArray = array_filter($urlArray);
+
+			// 获取控制器名
+			$controllerName = ucfirst($urlArray[0]);
+
+			// 获取动作名
+			array_shift($urlArray);
+			$actionName = $urlArray ? $urlArray[0] : $actionName;
+
+			// 获取url参数
+			array_shift($urlArray);
+			$param = $urlArray ? $urlArray : array();
+		}
+
+		// 判断控制器和操作是否存在
+		$controller = 'app\\controller\\' . $controllerName . 'controller';
+		if (!class_exists($controller)) {
+			exit($controller . '控制器不存在');
+		}
+		if (!method_exists($controller, $actionName)) {
+			exit($actionName . '方法不存在');
+		}
+
+		// 如果控制器和操作名存在，则实例化控制器，
+		// 因为控制器对象里面还会用到控制器和操作名，
+		// 所以实例化的时候吧他们两个的名称也传进去。
+		// 结合controller基类一起看
+		$dispatch = new $controller($controllerName, $actionName);
+
+		// $dispatch保存控制器实例化后的对象，
+		// 我们就可以调用他的方法，
+		// 也可以向方法中传入参数，
+		// 以下等同于：$dispatch->$actionName($param);
+		call_user_func_array(array($dispatch, $actionName), $param);
+	}
+
+	// 检测开发环境
+	public function setReporting()
+	{
+		error_reporting(E_ALL);
+		if (APP_DEBUG === true) {
+			ini_set('display_errors', 'On');
+		} else {
+			ini_set('display_errors', 'Off');
+			ini_set('log_errors', 'On');
 		}
 	}
-	
+
+	// 删除敏感字符
+	public function stripSlashesDeep($value)
+	{
+		$value = is_array($value) ? array_map(array($this, 'stripSlashesDeep'), $value) : stripslashes($value);
+		return $value;
+	}
+
+	// 检测敏感字符并删除
+	public function removeMagicQuotes()
+	{
+		if (get_magic_quotes_gpc()) {
+			$_GET = isset($_GET) ? $this->stripSlashesDeep($_GET) : '';
+			$_POST = isset($_POST) ? $this->stripSlashesDeep($_POST) : '';
+			$_COOKIE = isset($_COOKIE) ? $this->stripSlashesDeep($_COOKIE) : '';
+			$_SESSION = isset($_SESSION) ? $this->stripSlashesDeep($_SESSION) ? '';
+		}
+	}
+
+	// 检测自定义全局变量并移除。因为
+	// register_globals 已经弃用，如果已经弃用的
+	// register_globals 指令被设置为 on，那么局部
+	// 变量也将在脚本的全局作用域中可用。
+	// 例如，$_POST['foo'] 也将以 $foo 的形式
+	// 存在，这样写是不好的实现，会影响代码中的其
+	// 他变量。相关信息，
+	// 参考：http://php.net/manual/zh/faq.using.php#faq.register-globals
+	public function unregisterGlobals()
+	{
+		if (ini_get('register_globals')) {
+			$array = array('_SESSION', '_POST', '_GET', '_COOKIE', '_REQUEST', '_SERVER', '_ENV', '_FILES');
+			foreach ($array as $value) {
+				foreach ($GLOBALS[$value] as $key => $var) {
+					if ($var === $GLOBALS[$key]) {
+						unset($GLOBALS[$key]);
+					}
+				}
+			}
+		}
+	}
+
+	// 配置数据库信息
+	public function setDbConfig()
+	{
+		if ($this->config['db']) {
+			define('DB_HOST', $this->config['db']['host']);
+			define('DB_NAME', $this->config['db']['dbname']);
+			define('DB_USER', $this->config['db']['username']);
+			define('DB_PASS', $this->config['db']['password']);
+		}
+	}
+
+	// 自动加载类
+	public function loadClass($className)
+	{
+		$classMap = $this->classMap();
+
+		if (isset($classMap[$className])) {
+			// 包含内核文件
+			$file = $classMap[$className];
+		} elseif (strpos($className, '\\') !== false) {
+			// 包含应用（application目录）文件
+			$file = APP_PATH . str_reqlace('\\', '/', $className) . '.php';
+			if (!is_file($file)) {
+				return;
+			}
+		} else {
+			return;
+		}
+
+		include $file;
+
+		// 这里可以加入判断， 如果名为$className的类、接口或者性状不存在，则调试模式下抛出错误
+	}
+
+	// 内核文件命名空间映射关系
+	protected function classMap()
+	{
+		return [
+			'fastphp\base\Controller' => CORE_PATH . '/base/Controller.php',
+			'fastphp\base\Model' => CORE_PATH . '/base/Model.php',
+			'fastphp\base\View' => CORE_PATH . '/base/View.php',
+			'fastphp\db\Db' => CORE_PATH . '/db/Db.php',
+			'fastphp\db\Sql' => CORE_PATH . '/db/Sql.php',
+		];
+	}
 }
 
 
